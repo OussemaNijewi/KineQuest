988d6587950b3ddca016bd3d89a873c6495f4d98
Oussema Nijewi <oussemasn@gmail.com>
2026-09-20 15:33:35 -0400

    using unreal built in pointer instead of raw pointers - better memory management

--- Source/KineQuest/Public/Hands/VRHand.h
+++ Source/KineQuest/Public/Hands/VRHand.h
@@ -26,13 +26,13 @@ protected:
 
 	UPROPERTY(EditAnywhere, BlueprintReadOnly, Category = "Components")
 	//used to track the physical hardware in physical world and match that in VR world
-	UMotionControllerComponent* MotionController;
+	TObjectPtr<UMotionControllerComponent> MotionController;
 	//physical 3d representation of the hand and animations
 	UPROPERTY(EditAnywhere, BlueprintReadOnly, Category = "Components|Hands")
-	USkeletalMeshComponent* HandMesh;
+	TObjectPtr<USkeletalMeshComponent> HandMesh;
 	//used to let players interact with 3D UI menus in the game world
 	UPROPERTY(EditAnywhere, BlueprintReadOnly, Category = "Components|Hands")
-	UWidgetInteractionComponent* WidgetInteractionComponent;
+	TObjectPtr<UWidgetInteractionComponent> WidgetInteractionComponent;
 
 
 public:	
